4a92e05e02bd5783829d06e02578718eaec78d5e
motherofpugs <csampaidominika@gmail.com>
2024-02-18 19:28:29 +0100

    added base form

--- react-threejs-practice/src/pages/Contact.jsx
+++ react-threejs-practice/src/pages/Contact.jsx
@@ -1,7 +1,84 @@
-import React from "react";
-
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 const Contact = () => {
-  return <div>Contact</div>;
+  const formRef = useRef(null);
+
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleFocus = (e) => {};
+  const handleBlur = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    emailjs.sendForm();
+  };
+
+  return (
+    <section className="flex relative lg:flex:row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in touch</h1>
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Your name"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="youremail@email.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your message
+            <textarea
+              name="message"
+              className="input"
+              placeholder="Let me know how I can help you!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? "Sending.." : "Send message"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
