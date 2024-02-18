f9823ea45e578522ce9f636e0bfb558a7340876c
motherofpugs <csampaidominika@gmail.com>
2024-02-18 01:12:15 +0100

    added popup texts

---
+++ react-threejs-practice/src/components/HomeInfo.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+
+import { arrow } from "../assets/icons";
+
+const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 1)
+    return (
+      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+        Hi, I'm
+        <span className="font-semibold mx-2 text-white">Dominika</span>
+        🥰
+        <br />A Front End Developer from Hungary
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">
+          I enjoy learning new things and exploring innovations
+        </p>
+
+        <Link to="/about" className="neo-brutalism-white neo-btn">
+          Learn more
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className="info-box">
+        <p className="font-medium text-center sm:text-xl">
+          I've always loved working independently and <br /> experiencing things
+          on my own.
+        </p>
+
+        <Link to="/projects" className="neo-brutalism-white neo-btn">
+          Visit my portfolio
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">
+          I'm a junior, but I'm enthusiastic about my job <br /> and am always
+          ready to learn
+        </p>
+
+        <Link to="/contact" className="neo-brutalism-white neo-btn">
+          Let's talk
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+        </Link>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default HomeInfo;
--- react-threejs-practice/src/pages/Home.jsx
+++ react-threejs-practice/src/pages/Home.jsx
@@ -6,11 +6,7 @@ import Island from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
-{
-  /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        POPUP
-      </div> */
-}
+import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
@@ -47,13 +43,15 @@ const Home = () => {
 
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
   return (
-    <section
-      className={`w-full h-screen relative ${
-        isRotating ? "cursor-grabbing" : "cursor-grab"
-      }`}
-    >
+    <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
+
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
